d08a8451fa3e0e4e44e3fc4c45f65495d6e0c9ec
guodongxiaren <879231132@qq.com>
2021-05-30 12:14:15 +0800

    alias

--- alias.sh
+++ alias.sh
@@ -3,6 +3,14 @@ export TERM=xterm-256color # 防止tmux中使用删除键出问题
 export EDITOR=vim # 防止crontab -e用nano打开
 
 
+alias grep="grep --color"
+alias ls="ls --color"
+alias ll="ls -lhaF --color"
+alias tat="tmux a -t"
+alias tailf="tail -F"
+alias nb="rm -f nohup.out && nohup blade build &"
+
+
 git config --global core.editor vim # 防止git commit用nano打开
 
 # 生成某个文件的绝对路径，方便cp
