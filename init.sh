0437bdea490df82fd75f3ae8df3e0c7490e5c15d
果冻虾仁 <guodong@apache.org>
2025-04-19 13:53:13 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,9 @@ cd zsh-5.7.1 # 版本号可能变
 make -j8 && make install
 
 ### init oh-my-zsh
-ln -s dora/ohmyzsh/ ~/.oh-my-zsh
+# oh-my-zsh直接用官网命令，某些情况下在国内会可能下载不下来。可以先浏览器打开https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/refs/heads/master/tools/install.sh
+# 然后复制文本到远程机器上，保存install.sh并执行。
+# ln -s dora/ohmyzsh/ ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 chsh -s "$HOME/local/bin/zsh"
